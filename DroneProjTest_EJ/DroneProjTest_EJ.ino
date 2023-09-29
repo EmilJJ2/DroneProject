eb86ae96ca9c726da10c5be49aa5d50575ca492a
retrostyle012 <abhay.harpalani@gmail.com>
2023-09-29 16:25:35 -0500

    organized code

--- DroneProjTest_EJ/DroneProjTest_EJ.ino
+++ DroneProjTest_EJ/DroneProjTest_EJ.ino
@@ -1,78 +1,171 @@
 #include <Wire.h>
-const int MPU = 0x68; // MPU6050 I2C address
-float AccX, AccY, AccZ;
-float GyroX, GyroY, GyroZ;
-float accAngleX, accAngleY, gyroAngleX, gyroAngleY, gyroAngleZ;
+#include <Servo.h>
+
+const int MPU = 0x68; // mpu6050 i2c address
+const float mPI = 3.141592653;
+
+// translational
+float posX, posY, posZ; // xyz position
+float velX, velY, velZ; // velocity
+float accX, accY, accZ; // acceleration
+
+// angular
+float AngX, AngY, AngZ; // angle (around xyz axes)
+float AngVelX, AngVelY, AngVelZ; // angular velocity
+float AngAccX, AngAccY, AngAccZ; // angular acceleration
+
+Servo ESC;
+
+
+
+
+
+
+float GyroX, GyroY, GyroZ; // current value of gryoscope
+float accAngleX, accAngleY; // ?
+float gyroAngleX, gyroAngleY, gyroAngleZ;
 float roll, pitch, yaw;
-float AccErrorX, AccErrorY, GyroErrorX, GyroErrorY, GyroErrorZ;
+float accErrorX, accErrorY, gyroErrorX, gyroErrorY, gyroErrorZ;
 float elapsedTime, currentTime, previousTime;
 int c = 0;
 
 
-void setup() {
-  Serial.begin(1200);
-  Wire.begin();                      // Initialize comunication
-  Wire.beginTransmission(MPU);       // Start communication with MPU6050 // MPU=0x68
-  Wire.write(0x6B);                  // Talk to the register 6B
-  Wire.write(0x00);                  // Make reset - place a 0 into the 6B register       
-  Wire.endTransmission(true);        //end the transmission
-
-  // Write to Gyro config
-  Wire.beginTransmission(MPU);  
-  Wire.write(0x1B);
-  // 8 in Hex represents Bit3 and turns the gyro into 500 deg/s
-  // 0 resets to 250 deg/s (I THINK)
-  Wire.write(0x0); 
-  Wire.endTransmission();
-
-  // Write to Accel config
-  Wire.beginTransmission(MPU);  
-  Wire.write(0x1C);
-  Wire.write(0x0); 
-  Wire.endTransmission();
 
 
+
+// calculates trapezoidal integral
+class Integrator {
+	public:
+        // initialize integral to 0
+        Integrator(float startingVal) {
+            lastVal = startingVal;
+            integral = 0;
+        }
+        // add value to integral and update lastVal
+        // timeStep is time since last call to addValue
+        void addValue(float newVal, float timeStep) {
+            integral += timeStep * (lastVal + newVal)/2;
+            lastVal = newVal;
+        }
+        // get current value of integral
+        float getIntegral() {
+            return integral;
+        }
+	private:
+        // running integral
+        float integral;
+        // last value added to integral
+        float lastVal;
+};
+
+
+void setupServo() {
+    // (pin, min pulse width, max pulse width in microseconds)
+    // motor 1 attached to pin 9
+	ESC.attach(9, 1000, 2000);
+	Serial.begin(9600);
+    ESC.write(20);
 }
 
+// calculates derivative
+float getDerivative(float val, float lastVal, float timeStep) {
+	return (val - lastVal)/timeStep;
+}
+
+void getAccel() {
+	// read accelerometer data
+	Wire.beginTransmission(MPU);
+	Wire.write(0x3B); // start with register 0x3B (ACCEL_XOUT_H)
+	Wire.endTransmission(false);
+	Wire.requestFrom(MPU, 6, true); // read 6 registers total, each axis value is stored in 2 registers
+	// for a range of +-2g, we need to divide the raw values by 16384, according to the datasheet
+	AccX = (Wire.read() << 8 | Wire.read()) / 16384.0; // X-axis value
+	AccY = (Wire.read() << 8 | Wire.read()) / 16384.0; // Y-axis value
+	AccZ = (Wire.read() << 8 | Wire.read()) / 16384.0; // Z-axis value
+}
+
+// prepare to get gyro data
+void getGryo() {
+	Wire.beginTransmission(MPU);
+	Wire.write(0x43); // gyro data first register address 0x43
+	Wire.endTransmission(false);
+	Wire.requestFrom(MPU, 6, true); // read 4 registers total, each axis value is stored in 2 registers
+    // for a 250deg/s range we have to divide first the raw value by 131.0, according to the datasheet
+	// the higher the range, the less precise the returned degree value
+	GyroX = (Wire.read() << 8 | Wire.read()) / 131;
+	GyroY = (Wire.read() << 8 | Wire.read()) / 131;
+	GyroZ = (Wire.read() << 8 | Wire.read()) / 131;
+}
+
+void calcAccelAngle() {
+    // calculate roll and pitch
+	accAngleY = atan(-AccX/sqrt(AccY*AccY + AccZ*AccZ)) * 180 / mPI;
+	accAngleX = atan(AccY/sqrt(AccX*AccX + AccZ*AccZ)) * 180 / mPI;
+}
+
+void calcAngle() {
+    // gyro current angle calculate
+	// integrates the change in gyro angle over time
+	// deg = deg + (deg/s)*s
+	gyroAngleX = gyroAngleX + GyroX*elapsedTime;
+	gyroAngleY = gyroAngleY + GyroY*elapsedTime;
+}
+
+// calculates time between each loop for integral
+void calcTime() {
+	previousTime = currentTime;        // previous time is stored before the actual time read
+	currentTime = millis();            // current time actual time read
+	elapsedTime = (currentTime - previousTime) / 1000; // convert to seconds
+}
+
+void getInput() {
+    Serial.write("here we would get controller input");
+}
+
+
+void setup() {
+	Wire.begin();                      // initialize comunication
+	Wire.beginTransmission(MPU);       // start communication with MPU6050 // MPU=0x68
+	Wire.write(0x6B);                  // talk to the register 6B
+	Wire.write(0x00);                  // make reset - place a 0 into the 6B register
+	Wire.endTransmission(true);        // end  transmission
+
+	// write to Gyro config
+	Wire.beginTransmission(MPU);
+	Wire.write(0x1B);
+	// 8 in hex represents Bit3 and turns the gyro into 500 deg/s
+	// 0 resets to 250 deg/s (I THINK)
+	Wire.write(0x0);
+	Wire.endTransmission();
+
+	// write to accel config
+	Wire.beginTransmission(MPU);
+	Wire.write(0x1C);
+	Wire.write(0x0);
+	Wire.endTransmission();
+}
+
+
 void loop() {
-  float mPI = 3.141592653;
+	// updates elapsedTime
+	calcTime();
 
-  // TIME TRACKER
-  previousTime = currentTime;        // Previous time is stored before the actual time read
-  currentTime = millis();            // Current time actual time read
-  elapsedTime = (currentTime - previousTime) / 1000; // Convert to seconds
-  
-  // === Read accelerometer data === //
-  Wire.beginTransmission(MPU);
-  Wire.write(0x3B); // Start with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
-  //For a range of +-2g, we need to divide the raw values by 16384, according to the datasheet
-  AccX = (Wire.read() << 8 | Wire.read()) / 16384.0; // X-axis value
-  AccY = (Wire.read() << 8 | Wire.read()) / 16384.0; // Y-axis value
-  AccZ = (Wire.read() << 8 | Wire.read()) / 16384.0; // Z-axis value
+    // stores values in accX, accY, accZ
+	getAccel();
 
-  // Calculate Roll and Pitch
-  accAngleY = atan(-AccX/sqrt(AccY*AccY + AccZ*AccZ)) * 180 / mPI;
-  accAngleX = atan(AccY/sqrt(AccX*AccX + AccZ*AccZ)) * 180 / mPI;
-  
-  
-  Wire.beginTransmission(MPU);
-  Wire.write(0x43); // Gyro data first register address 0x43
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 6, true); // Read 4 registers total, each axis value is stored in 2 registers
-  GyroX = (Wire.read() << 8 | Wire.read()) / 131; // For a 250deg/s range we have to divide first the raw value by 131.0, according to the datasheet
-  GyroY = (Wire.read() << 8 | Wire.read()) / 131; // The higher the range, the less precise the returned degree value
-  GyroZ = (Wire.read() << 8 | Wire.read()) / 131;
+    // calculates accAngleX and accAngleY
+	calcAccelAngle();
 
-  // Gyro current Angle Calculate
-  // Integrates the change in gyro angle over time 
-  // deg = deg + (deg/s)*s
-  gyroAngleX = gyroAngleX + GyroX*elapsedTime; 
-  gyroAngleY = gyroAngleY + GyroY*elapsedTime;
-  
-  // COMPLIMENTARY FILTER
-  roll = 0.99 * gyroAngleX + 0.01 * accAngleX;
-  Serial.println();
+    // prepare to get gyro data
+	getGyro();
 
+    // calculates gyroAngleX and gyroAngleY
+	calcAngle();
+
+	// complimentary filter
+	roll = 0.99 * gyroAngleX + 0.01 * accAngleX;
+
+    getInput();
+
+	Serial.println();
 }
