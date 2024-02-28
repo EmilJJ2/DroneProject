9cd82f0160b831f42cb51074d6b6eac9af0b7ec3
Emil Jermann <emiljj2@illinois.edu>
2024-02-28 09:25:18 -0600

    VelZ integrated and rotated, hover and setdown implemented, state variables set

--- DroneProjTest_EJ/DroneClasses/Integrator.cpp
+++ DroneProjTest_EJ/DroneClasses/Integrator.cpp
@@ -3,7 +3,6 @@
 Integrator::Integrator() {
     lastVal = 0;
     integral = 0;
-
 }
 
 Integrator::Integrator(float startingVal) {
@@ -14,6 +13,13 @@ Integrator::Integrator(float startingVal) {
 // add value to integral and update lastVal
 // timeStep is time since last call to addValue
 void Integrator::addValue(float newVal, float timeStep) {
+    integral += timeStep * (lastVal + newVal)/2; // Mid Point Reimann Sum??
+    lastVal = newVal;
+}
+
+void Integrator::addRotatedValue(float x, float y, float z, float pitch, float roll, float timeStep) {
+    float newVal = -x*sin(pitch) + y*cos(pitch)*sin(roll) + z*cos(pitch)*cos(roll);
+    
     integral += timeStep * (lastVal + newVal)/2; // Mid Point Reimann Sum??
     lastVal = newVal;
 }
